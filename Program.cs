ea2103dc740ab4efe51f30df5925a1990d0a46eb
Khujrat Shaikh <76805503+khujrat17@users.noreply.github.com>
2023-09-25 11:18:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,306 @@
+﻿namespace logicalprogram
+{
+    static class Allpattern
+    {
+
+        public static void Pattern1()
+        {
+            try
+            {
+
+
+                Console.WriteLine(" Enter your Range");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+
+                        Console.Write(i + "");
+                    Console.WriteLine();
+
+
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void Pattern2()
+        {
+            try
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("This is pttern no 2 ");
+                Console.WriteLine(" Enter your Range");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+
+                        Console.Write(j + "");
+                    Console.WriteLine();
+
+
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void Pattern2(int x)
+        {
+            try
+            {
+
+
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("This is pattern no 3");
+                Console.WriteLine(" Enter your Range");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = i; j >= 1; j--)
+
+                        Console.Write(i + "");
+                    Console.WriteLine();
+
+
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void Pattern2(string y)
+        {
+            try
+            {
+
+
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("This is pattern no 3");
+                Console.WriteLine(" Enter your Range");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = i; j >= 1; j--)
+
+                        Console.Write(j + "");
+                    Console.WriteLine();
+
+
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void alphabetpattern1()
+        {
+            try
+            {
+
+
+                Console.WriteLine("--------------------------");
+                Console.WriteLine(" This is alphabetical pattern 1 :");
+                Console.WriteLine(" Enter a range:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-----");
+
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+
+                        Console.Write((char)(i + 64));
+                    Console.WriteLine(" ");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        public static void alphabetpattern2()
+        {
+            try
+            {
+
+
+                Console.WriteLine("-----------");
+                Console.WriteLine("This is alphabet pattern no 2:");
+                Console.WriteLine("Enter a range:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                        Console.Write((char)(j + 64));
+                    Console.WriteLine(" ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void alphabetpattern3()
+        {
+            try
+            {
+
+
+                Console.WriteLine("---------");
+                Console.WriteLine("This is alphabet pattern no 3:");
+                Console.WriteLine(" Enter a range:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-----");
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = i; j >= 1; j--)
+                        Console.Write((char)(i + 64));
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+        }
+        public static void alphabetpattern4()
+        {
+            try
+            {
+
+
+                Console.WriteLine("---------");
+                Console.WriteLine("This is alphabet pattern no 4:");
+                Console.WriteLine(" Enter a range:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-----");
+                for (int i = n; i >= 1; i--)
+                {
+                    for (int j = i; j >= 1; j--)
+                        Console.Write((char)(j + 64));
+                    Console.WriteLine();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void alphabetpattern5()
+        {
+            try
+            {
+
+
+                Console.WriteLine("----------");
+                Console.WriteLine("This is alphabet pattern no 5:");
+                Console.WriteLine("Enter a range :");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("-------");
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write((char)(j + 64));
+                    }
+                    for (int j = i - 1; j >= 1; j--)
+                    {
+                        Console.Write((char)(j + 64));
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+        public static void alkphabetpattern6()
+        {
+            try
+            {
+
+
+                Console.WriteLine("-------------");
+                Console.WriteLine("This is alphabet pattern no 6:");
+                Console.WriteLine("Enter a range:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("------");
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= n - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write((char)(i + 64));
+                    }
+                    for (int j = i - 1; j >= 1; j--)
+                    {
+                        Console.Write((char)(i + 64));
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+
+
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+            Allpattern.Pattern1();
+            Allpattern.Pattern2();
+            Allpattern.Pattern2(0);
+            Allpattern.Pattern2("");
+            Allpattern.alphabetpattern1();
+            Allpattern.alphabetpattern2();
+            Allpattern.alphabetpattern3();
+            Allpattern.alphabetpattern4();
+            Allpattern.alphabetpattern5();
+            Allpattern.alkphabetpattern6();
+
+        }
+    }
+}
---
+++ Stringlogic.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace logicalprogram
+{
+    class String
+    {
+        // SpeechSynthesizer sy = new SpeechSynthesizer();
+
+
+
+        public void reverse()
+        {
+
+
+
+            Console.WriteLine(" Enter a string which you want to reverse");
+            string str = Console.ReadLine();
+            string rev = " ";
+            for (int i = str.Length - 1; i > 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            Console.WriteLine(rev);
+        }
+        public void stringlength()
+        {
+
+            Console.WriteLine("============================");
+            Console.WriteLine(" Enter a string ");
+            string st = Console.ReadLine();
+            Console.WriteLine(" The length of the string is: " + st.Length);
+        }
+
+        public void reversenumber()
+        {
+
+            Console.WriteLine("=============================");
+            int num, rev = 0, rem;
+            Console.WriteLine(" Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num /= 10;
+            }
+            // sy.Speak(" Chal aati ky 9 se 12 ");
+
+            Console.WriteLine(" reversed number is:" + rev);
+
+        }
+        public void reverseword()
+        {
+            Console.WriteLine("=========================");
+            int sk;
+            Console.WriteLine(" Enter a string ");
+            string str = Console.ReadLine();
+            string[] a = str.Split(' ');
+            int k = a.Length - 1;
+            sk = k;
+            for (int i = k; sk >= 0; k--)
+            {
+                Console.Write(a[sk] + "" + ' ');
+                --sk;
+            }
+            Console.ReadKey();
+
+        }
+        public void duplicatearray()
+        {
+            Console.WriteLine("======================");
+            int[] arr = new int[] { 1, 2, 2, 3, 4, 56, 7, 56, 78, 87, 78 };
+            Console.WriteLine(" Duplicates elements in array are:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.WriteLine(arr[j]);
+                }
+            }
+
+        }
+
+
+
+    }
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            // SpeechSynthesizer sy = new SpeechSynthesizer();
+            String sn = new String();
+            //sn.reverse();
+            //sn.stringlength();
+            //sn.reversenumber();
+            //sn.reverseword();
+            sn.duplicatearray();
+
+
+        }
+
+    }
+
+}
+
